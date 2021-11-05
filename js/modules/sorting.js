b22cf135f32acadc0c43bbab8be289a2c024a497
veselyiJoker <60154163+veselyiJoker@users.noreply.github.com>
2021-11-05 18:23:06 +0300

    Add files via upload

---
+++ js/modules/sorting.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import { debounce } from "./common.js";
+import { renderCatalog } from "./renderCatalog.js";
+
+const catalogDataSorted = [];
+const sortBtns = document.querySelectorAll('.sorting__order-tab input[name=sorting-order]');
+
+export const sort = (type, catalogData) => {
+    catalogDataSorted.splice(0, catalogDataSorted.length, ...catalogData);
+
+    switch (type) {
+      case 'popular':
+        return catalogDataSorted;
+
+      case 'cheap':
+        return catalogDataSorted.sort((activeItem, nextItem) => activeItem.price - nextItem.price);
+
+      case 'new':
+        return catalogDataSorted.sort((activeItem, nextItem) => nextItem.publishDate - activeItem.publishDate);
+    }
+}
+
+export const initSortBtns = (catalogData) => {
+    catalogDataSorted.splice(0, catalogDataSorted.length, ...catalogData);
+
+    sortBtns.forEach(elem => {
+        elem.addEventListener('click', debounce( (e) => {
+                renderCatalog(sort(e.target.value, catalogData));
+            })
+        );
+    });
+}
+
+export const getCatalogDataSorted = () => {
+  return catalogDataSorted;
+}
+
+export const getSortBtns = () => {
+  return sortBtns;
+}
